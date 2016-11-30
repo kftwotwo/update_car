a3b8489b8aac5cb48c6261cdfff815fb6a403876
Kevin Finley <Kevin@Kevins-MBP.lan>
2016-11-29 20:36:43 -0800

    practice with play time

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -21,15 +21,21 @@ class Vehicle
     @year
   end
 
-  define_method(:id) do
-    @id
-  end
-
   define_singleton_method(:all) do
     @@cars
   end
 
   def save
     @@cars.push(self)
+  end
+
+  define_singleton_method(:find) do |id|
+    found_cars = nil
+    @@cars.each() do |cars|
+      if cars.id().eql?(id)
+        found_cars = cars
+      end
+    end
+    found_cars
   end
 end
